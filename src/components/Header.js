b8ff2a8124dfcbaa9c2f16dbb4f406a0b96bcaa1
uk974q <ullasm@gmail.com>
2023-09-30 23:10:13 +0530

    FIX: flex added instead of grid, cors string added for future

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,21 +31,23 @@ const Header = () => {
     }
 
     return(
-        <div className="grid grid-flow-col col-span-12 h-16 mb-2 items-center p-2 shadow-lg">
-            <div className="h-12 flex items-center col-span-2">
+        <div className="flex h-16 mb-2 items-center p-2 shadow-lg">
+            <div className="h-12 flex items-center mr-2 ">
                 <i onClick={() => dispatchAction(toggleSlice())} className="las la-bars text-lg cursor-pointer"></i>
                 <Link to={"/"}>
-                    <img alt="youtubelogo" className="ml-5 " height="10px" width="70px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/1280px-YouTube_Logo_%282013-2017%29.svg.png" />
+                    <img alt="youtubelogo" className="ml-5 " height="10px" width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/1280px-YouTube_Logo_%282013-2017%29.svg.png" />
                 </Link>
             </div>
-            <div className="h-12 flex items-center w-full col-span-8 justify-center">
-                <div className="relative w-1/2 h-2/3 py-0">
-                    <input placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="text" className="py-0 px-5 h-full w-full rounded-l-full border-gray-200 border-2"/>
-                    <Autosuggest suggestions={suggestions}/>
+            <div className="h-12 flex-grow mx-auto flex justify-center">
+                <div className="h-2/3 w-2/3 py-0 flex my-auto">
+                    <div className="relative flex-grow" >
+                        <input placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="text" className="py-0 px-5 h-full w-full rounded-l-full border-gray-200 border-2"/>
+                        <Autosuggest suggestions={suggestions}/>
+                    </div>
+                    <button className="rounded-r-full py-0 px-3 text-sm bg-gray-100 text-black"><i className="las la-search"></i></button>
                 </div>
-                <button className="rounded-r-full py-0 px-3 h-2/3 text-sm bg-gray-100 text-black"><i className="las la-search"></i></button>
             </div>
-            <i className="las la-user-circle text-4xl cursor-pointer col-span-2 text-center"></i>
+            <i className="las la-user-circle text-4xl cursor-pointer flex-grow-0 text-center self-end ml-auto"></i>
         </div>
     )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const API_KEY = "AIzaSyCUntVJzk9--oL8xSoEkkSwlmgsD5htiRI"
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=90&regionCode=IN&key=${API_KEY}`
 export const YOUTUBE_AUTOSUGGEST = "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q="
 export const YOUTUBE_VIDEO = "https://www.youtube.com/embed/"
-
+export const CORS_MSG = "Please enable CORS"
 export const LIVE_CHAT_OFFSET = 100
 export const CHAT_NAMES = ["Alice", "Bob", "Charlie", "David", "Eve"];
 export const CHAT_MESSAGES = [
